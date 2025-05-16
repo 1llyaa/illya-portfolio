ba90ba96e3d22a96cbfe06af1780e5eb457491bb
1llyaa <illyamiloserdov@skaut.cz>
2025-05-16 20:15:56 +0200

    remove 3d graphics from hero component

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BackgroundSceneComponent } from '../background-scene/background-scene.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [CommonModule, BackgroundSceneComponent],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {}
